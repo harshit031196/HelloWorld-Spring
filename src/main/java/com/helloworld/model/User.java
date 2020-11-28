3d832986358d07be17c0db233143ca6d03bcbb37
harshit031196 <guptaharshit0311@gmail.com>
2020-11-28 07:25:27 +0530

    Added the hello world in spring

---
+++ src/main/java/com/helloworld/model/User.java
@@ -0,0 +1,13 @@
+package com.helloworld.model;
+
+public class User {
+	private String firstName;
+	private String lastName;
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+}
